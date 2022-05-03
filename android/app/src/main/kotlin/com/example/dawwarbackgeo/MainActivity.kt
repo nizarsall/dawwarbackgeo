a65400b5cc4430379cb42e89e8c0295257fc583a
nizar salloum <nizarsalloumpr@gmail.com>
2022-05-03 20:15:35 +0300

    initial

---
+++ android/app/src/main/kotlin/com/example/dawwarbackgeo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dawwarbackgeo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/MapView.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Mapview extends StatefulWidget {
+  const Mapview({ Key? key }) : super(key: key);
+
+  @override
+  State<Mapview> createState() => _MapviewState();
+}
+
+class _MapviewState extends State<Mapview> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
